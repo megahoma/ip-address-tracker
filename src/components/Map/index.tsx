b0dafbff89b1f13b8dc258ec9da39a52045d597c
megahoma <timkrut5348@gmail.com>
2021-12-18 19:07:12 +0300

    changed marker on the map

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -2,7 +2,13 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import { State } from '../../types'
 import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet'
+import { icon } from 'leaflet'
 import { Container } from './styles'
+
+const LeafletIcon = icon({
+  iconUrl: './images/icon-location.svg',
+  iconSize: [30, 40],
+})
 
 const Map = () => {
   const geo = useSelector((state: State) => state.geo)
@@ -25,10 +31,11 @@ const Map = () => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[geo.location.lat, geo.location.lng]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
+        <Marker
+          icon={LeafletIcon}
+          position={[geo.location.lat, geo.location.lng]}
+        >
+          <Popup>{geo.isp}</Popup>
         </Marker>
         <CenterMap coords={[geo.location.lat, geo.location.lng]} />
       </MapContainer>
